242a09f9313e71d44d45f900c18422c78f615150
KhoaPC <luongvankhoapc@gmail.com>
2023-03-01 09:21:33 +0700

    23Mar01 Use svg file

--- components/Add.js
+++ components/Add.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { useState } from 'react';
 import styles from '../styles/Navbar.module.css';
+import Icon from './assets/icon-add.js';
 import CreateCollection from './Create-collection';
 
 function Add() {
@@ -10,13 +11,7 @@ function Add() {
         <>
             <div className={styles.container_add}>
                 <div className={styles.icon}>
-                    <svg width="24" height="24" viewBox="0 0 39 24" fill="green">
-                        <g transform="scale(0.03125 0.03125)">
-                            <path
-                                d="M992 384h-352v-352c0-17.672-14.328-32-32-32h-192c-17.672 0-32 14.328-32 32v352h-352c-17.672 0-32 14.328-32 32v192c0 17.672 14.328 32 32 32h352v352c0 17.672 14.328 32 32 32h192c17.672 0 32-14.328 32-32v-352h352c17.672 0 32-14.328 32-32v-192c0-17.672-14.328-32-32-32z">
-                            </path>
-                        </g>
-                    </svg>
+                    <Icon />
                 </div>
                 <div className={styles.menu}>
                     <Link className={styles.menu_item} href="/create">Flashcard</Link>
@@ -24,7 +19,7 @@ function Add() {
                     <div className={styles.menu_item} >Topic</div>
                 </div>
             </div>
-            {show ? <CreateCollection/> : ''}
+            {show ? <CreateCollection /> : ''}
         </>
 
 
@@ -32,3 +27,5 @@ function Add() {
 }
 
 export default Add;
+
+
