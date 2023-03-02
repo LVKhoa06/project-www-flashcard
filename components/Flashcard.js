9c20c4ce16350622ac059b524875abf36e479069
KhoaPC <luongvankhoapc@gmail.com>
2023-03-02 15:16:04 +0700

    23Mar02 Create flashcard component

---
+++ components/Flashcard.js
@@ -0,0 +1,82 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import styles from '../styles/Home.module.css';
+import IconMenu from './assets/icon-menu';
+import MenuFlashcard from './MenuFlashcard';
+
+function Flashcard(props) {
+    const {data, setData, listTopic} = props
+    const [session, setSession] = useState({ value: '', turn: '' });
+    const [showMenu, setShowMenu] = useState(false);
+    const [curId, setCurId] = useState(0);
+
+    useEffect(() => {
+        const menu = document.querySelector(`#menu-${curId}`);
+        const elm = document.querySelector('.show');
+
+        if (showMenu) {
+            elm?.classList.remove('show')
+            menu?.classList.remove('show');
+        }
+        else menu?.classList.add('show');
+    }, [showMenu, curId])
+
+    const updateFlashcardHandler = async (value, field, id) => {
+        await axios.patch(
+            'api/home',
+            {
+                id,
+                field,
+                value,
+            },
+            {
+                "Content-Type": "application/json",
+            }
+        );
+        console.log('Update');
+    } // updateFlashcardHandler
+    
+    return (
+        <>
+         {data.map(item => {
+                    const id = item.id;
+                    const topicName = listTopic.find(topic => topic.topic_id == item.topic_id)?.topic;
+
+                    return (
+                        <div id={id} /*title={topicName}*/ className={styles.flashcard} key={id}>
+                            <div>
+                                <h2
+                                    // disable warning when use contentEditable
+                                    suppressContentEditableWarning={true}
+                                    onKeyUp={(e) => setSession({ turn: 'term', value: e.target.innerText })}
+                                    contentEditable
+                                >
+                                    {item.term}
+                                </h2>
+                            </div>
+                            <div>
+                                <span
+                                    suppressContentEditableWarning={true}
+                                    onKeyUp={(e) => setSession({ turn: 'description', value: e.target.innerText })}
+                                    contentEditable
+                                >
+                                    {item.description}
+                                </span>
+                            </div>
+                            <button className={''} onClick={() => updateFlashcardHandler(session.value, session.turn, id)}>Ok</button>
+                            <div
+                                onClick={(e) => {
+                                    setCurId(id);
+                                    setShowMenu(!showMenu);
+                                }} className={styles['menu-container']}>
+                                <IconMenu width='20px' height='20px' />
+                                <MenuFlashcard id={id} setShowMenu={setShowMenu} setData={setData}/>
+                            </div>
+                        </div>
+                    );
+                })}
+        </>
+    );
+}
+
+export default Flashcard;
--- components/pages/Home.js
+++ components/pages/Home.js
@@ -1,45 +1,13 @@
-import axios from 'axios';
 import Head from 'next/head';
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import styles from '../../styles/Home.module.css';
-import IconMenu from '../assets/icon-menu';
-import Collection from '../Collection';
-import MenuFlashcard from '../MenuFlashcard';
+import Flashcard from '../Flashcard';
 import SortAndFilter from '../SortAndFilter';
 
 function Home() {
     const configUseSortAndFilter = { sort: true, filter: true }
     const [data, setData] = useState([]);
-    const [session, setSession] = useState({ value: '', turn: '' });
     const [listTopic, setListTopic] = useState([]);
-    const [showMenu, setShowMenu] = useState(false);
-    const [curId, setCurId] = useState(0);
-
-    useEffect(() => {
-        const menu = document.querySelector(`#menu-${curId}`);
-        const foo = document.querySelector('.show');
-
-        if (showMenu) {
-            foo?.classList.remove('show')
-            menu?.classList.remove('show');
-        }
-        else menu?.classList.add('show');
-    }, [showMenu, curId])
-    
-    const updateFlashcardHandler = async (value, field, id) => {
-        await axios.patch(
-            'api/home',
-            {
-                id,
-                field,
-                value,
-            },
-            {
-                "Content-Type": "application/json",
-            }
-        );
-        console.log('Update');
-    } // updateFlashcardHandler
 
     return (
         <>
@@ -48,43 +16,7 @@ function Home() {
             </Head>
             <SortAndFilter use={configUseSortAndFilter} setData={setData} listTopic={listTopic} setListTopic={setListTopic} />
             <div className={styles.container}>
-                {data.map(item => {
-                    const id = item.id;
-                    const topicName = listTopic.find(topic => topic.topic_id == item.topic_id)?.topic;
-
-                    return (
-                        <div id={id} /*title={topicName}*/ className={styles.flashcard} key={id}>
-                            <div>
-                                <h2
-                                    // disable warning when use contentEditable
-                                    suppressContentEditableWarning={true}
-                                    onKeyUp={(e) => setSession({ turn: 'term', value: e.target.innerText })}
-                                    contentEditable
-                                >
-                                    {item.term}
-                                </h2>
-                            </div>
-                            <div>
-                                <span
-                                    suppressContentEditableWarning={true}
-                                    onKeyUp={(e) => setSession({ turn: 'description', value: e.target.innerText })}
-                                    contentEditable
-                                >
-                                    {item.description}
-                                </span>
-                            </div>
-                            <button className={''} onClick={() => updateFlashcardHandler(session.value, session.turn, id)}>Ok</button>
-                            <div
-                                onClick={(e) => {
-                                    setCurId(id);
-                                    setShowMenu(!showMenu);
-                                }} className={styles['menu-container']}>
-                                <IconMenu width='20px' height='20px' />
-                                <MenuFlashcard id={id} setShowMenu={setShowMenu} setData={setData}/>
-                            </div>
-                        </div>
-                    );
-                })}
+                <Flashcard data={data} setData={setData} listTopic={listTopic} />
             </div>
         </>
     )
