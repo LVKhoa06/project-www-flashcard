700f10532e376a43bb4882055a2b2e5ab995d8d0
KhoaPC <luongvankhoapc@gmail.com>
2023-03-10 18:43:22 +0700

    23Mar10 STYLES create page

--- components/pages/Create.js
+++ components/pages/Create.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import formatDateWithLocale from "utils/format-date.";
 import Head from "next/head";
+import styles from '../../styles/CreateFlashcard.module.css'
 
 
 function Create() {
@@ -28,7 +29,7 @@ function Create() {
 
     async function handlerCreate(e) {
         e.preventDefault();
-        let post = listFlashcard.find( item =>  item.term === term)
+        let post = listFlashcard.find(item => item.term === term)
 
         if (term === '') {
             return console.log('Please enter Term');
@@ -56,35 +57,39 @@ function Create() {
         } else {
             console.log('Term already exist');
         }
-
-
     } // handlerCreate
 
     return (
-
         <>
             <Head>
                 <title>Create Flashcard</title>
             </Head>
-            <form>
-                <div>
-                    <select
-                        onChange={(e) => setTopicId(e.target.value)}
-                    >
-                        <option value={-1}>Khác</option>
+            <div className={styles.container}>
+                <h3>Create a new flashcard</h3>
+                <form className={styles} >
+                    <div className={styles['container-option']} >
+                        <span>Topic:</span>
+                        <select
+                            onChange={(e) => setTopicId(e.target.value)}
+                        >
+                            <option value={-1}>Khác</option>
 
-                        {listTopic.map(item => {
-                            return item.topic_id > 0 ? <option key={item.topic_id} value={item.topic_id}>{item.topic}</option> : ''
+                            {listTopic.map(item => {
+                                return item.topic_id > 0 ? <option key={item.topic_id} value={item.topic_id}>{item.topic}</option> : ''
 
-                        })}
-                    </select>
+                            })}
+                        </select>
+                    </div>
+                    <div className={styles['container-input']}>
+                        <input className={styles['input-term']} value={term} onChange={(e) => setTerm(e.target.value)} placeholder="Term" />
+                        <textarea className={styles['input-desc']} value={description} onChange={(e) => setDiscription(e.target.value)} placeholder="Description" />
+                        <div className={styles['container-button']}>
+                            <button className={styles} onClick={(e) => handlerCreate(e)}>Create</button>
+                        </div>
+                    </div>
 
-                </div>
-                <input value={term} onChange={(e) => setTerm(e.target.value)} placeholder="Thuật ngữ" />
-                <input value={description} onChange={(e) => setDiscription(e.target.value)} placeholder="Giải thích" />
-
-                <button onClick={(e) => handlerCreate(e)}>submit</button>
-            </form>
+                </form>
+            </div>
         </>
 
     );
