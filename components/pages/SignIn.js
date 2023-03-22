a70699d8c2db13218274d6dcece9c4d94280359e
KhoaPC <luongvankhoapc@gmail.com>
2023-03-22 10:31:39 +0700

    23Mar22 UPDATE form notification

--- components/pages/SignIn.js
+++ components/pages/SignIn.js
@@ -1,6 +1,7 @@
 import { signIn } from "next-auth/react";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useRef, useState } from "react";
 import styles from "../../styles/Sign-in.module.scss";
@@ -18,24 +19,21 @@ export default function SignIn() {
   const refUser = useRef();
   const router = useRouter();
   const redirect_to = router.query?.redirect_to;
-  console.log(redirect_to);
+  const [condition, setCondition] = useState({ code: STATUS.UNKNOWN, message: "" });
 
-  async function signInHandler() {
-    const nickname = refUser.current.value.trim();
+  const signInHandler = async () => {
+    const username = refUser.current.value.trim();
     const password = refPass.current.value.trim();
 
-    if (!nickname && !password)
-      return;
-
-    if (!nickname)
-      return;
+    if (!username)
+      return setCondition({ code: STATUS.ERROR, message: 'Please enter your username' });
 
     if (!password)
-      return;
+      return setCondition({ code: STATUS.ERROR, message: 'Please enter your password' });
 
     const result = await signIn("credentials", {
       redirect: false,
-      nickname,
+      nickname: username,
       password,
     });
 
@@ -45,6 +43,7 @@ export default function SignIn() {
     else if (ok) {
       router.replace('/');
     }
+    setCondition({ code: STATUS.ERROR, message: error });
   } // signInHandler
 
   const keyEnterHandler = (event) => {
@@ -70,22 +69,33 @@ export default function SignIn() {
       <Head>
         <title>Sign-in</title>
       </Head>
-      <div className={styles.login_box}>
-        <h2>Login</h2>
-        <div className={styles.form}>
-          <div className={styles.user_box}>
-            <input placeholder="Nickname" onKeyDown={(event) => keyEnterHandler(event)} ref={refUser} type="text" />
-          </div>
-          <div className={styles.user_box}>
-            <input placeholder="Password" onKeyDown={(event) => keyEnterHandler(event)} ref={refPass} type="password" />
-          </div>
-          <div className={styles.contain_btn}>
-            <button onClick={signInHandler}>
-              Sign in
-            </button>
-            <button>
-              Register
-            </button>
+      <div className={styles.overlay}>
+        <div className={styles.login_box}>
+          <h2>Login</h2>
+          <div className={styles.form}>
+            <div className={styles.user_box}>
+              <input placeholder="Username" onKeyDown={(event) => keyEnterHandler(event)} ref={refUser} type="text" />
+            </div>
+            <div className={styles.user_box}>
+              <input placeholder="Password" onKeyDown={(event) => keyEnterHandler(event)} ref={refPass} type="password" />
+            </div>
+            <span className={`${condition.code === -1 ?
+              styles.unknown :
+              condition.code < 1 ?
+                styles.error :
+                styles.success
+              } 
+          ${styles.notification}`}>{condition.message}</span>
+            <div className={styles.contain_btn}>
+              <button onClick={signInHandler}>
+                Sign-in
+              </button>
+              <button>
+                <Link href="/sign-up">
+                  {'Sign-up>'}
+                </Link>
+              </button>
+            </div>
           </div>
         </div>
       </div>
