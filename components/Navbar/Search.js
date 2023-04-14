e4dea954179924de49692be57b31766fccca5c94
KhoaPC <luongvankhoapc@gmail.com>
2023-04-14 09:47:16 +0700

    23Apr14 UPDATE search flashcard

--- components/Navbar/Search.js
+++ components/Navbar/Search.js
@@ -1,7 +1,7 @@
 import IconSearch from 'assets/icon-search';
 import axios from 'axios';
 import { useRouter } from 'next/router';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import useDebounce from 'utils/useDebounce';
 import styles from '../../styles/Search.module.scss';
 import FlashcardDetail from '../flashcard/Detail';
@@ -9,10 +9,11 @@ import { useSession } from 'next-auth/react';
 
 function Search() {
     const [value, setValue] = useState('');
-    const [result, setResult] = useState([]);
-    const [isFocus, setIsFocus] = useState(false);
+    const [searchResult, setSearchResult] = useState([]);
+    const [flashcard, setFlashcard] = useState({});
     const [show, setShow] = useState(false);
-    const [data, setData] = useState({});
+    const [isFocus, setIsFocus] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
     const router = useRouter();
     const debouncedValue = useDebounce(value, 1500);
     const { data: session, status } = useSession();
@@ -20,7 +21,7 @@ function Search() {
     useEffect(() => {
         const closeElm = () => setIsFocus(false);
         document.addEventListener('click', closeElm);
-        
+
         return () => document.removeEventListener('click', closeElm)
     }, [])
 
@@ -31,9 +32,9 @@ function Search() {
         const handler = async () => {
             if (debouncedValue.length) {
                 const data = await axios.get(`/api/flashcard/search?key=${debouncedValue}`);
-                setResult(data.data);
-
-            } else setResult([])
+                setSearchResult(data.data);
+                setIsLoading(false);
+            }
         }
         handler();
     }, [debouncedValue])
@@ -56,7 +57,8 @@ function Search() {
                         value={value}
                         onChange={(e) => {
                             setValue(e.target.value)
-                            setResult([])
+                            setSearchResult([])
+                            setIsLoading(true);
                         }}
 
                         className={styles.input}
@@ -69,29 +71,30 @@ function Search() {
                     }} className={styles.button} type="submit"><IconSearch /></button>
                 </div>
                 <div
-                    className={`${styles.container_result} ${value.length && isFocus ? 'show' : ''} ${value.length && isFocus && !result.length ? 'flicker' : ''}`}
+                    className={`${styles.container_result} ${value.length && isFocus ? 'show' : ''} ${value.length && isFocus && !searchResult.length ? 'flicker' : ''}`}
                 >
-                    {!result.length ? <p>Looking for flashcards matching ' {value} '</p> : ''}
-                    {
-                        result.map(item => {
-                            return (
-                                <div onClick={() => {
-                                    setData(item);
-                                    setShow(true);
-                                }} className={styles.search_item} key={item.id}>
-                                    <div className={styles.left_item}>
-                                        <h3>{item.term}</h3>
-                                        <span>{item.description}</span>
+                    {isLoading ?
+                        <p>Looking for flashcards matching ' {value} '</p> :
+                        searchResult.length ?
+                            searchResult.map(item => {
+                                return (
+                                    <div onClick={() => {
+                                        setFlashcard(item);
+                                        setShow(true);
+                                    }} className={styles.search_item} key={item.id}>
+                                        <div className={styles.left_item}>
+                                            <h3>{item.term}</h3>
+                                            <span>{item.description}</span>
+                                        </div>
+                                        <div className={styles.right_item}>
+                                            <span>{item.topic}</span>
+                                        </div>
                                     </div>
-                                    <div className={styles.right_item}>
-                                        <span>{item.topic}</span>
-                                    </div>
-                                </div>
-                            )
-                        })
+                                )
+                            }) :
+                            <p>No flashcards found matching ' {value} '</p>
                     }
-                    {!result.length}
-                    {show && data && <FlashcardDetail show={show} setShow={setShow} data={data} />}
+                    {show && flashcard && <FlashcardDetail show={show} setShow={setShow} data={flashcard} />}
                 </div>
             </div>
 
